c81feae04940b279835f88478b94883756076a1b
tiago369 <tiagobarreto581@gmail.com>
2022-01-04 07:28:09 -0300

    v1

---
+++ dobot_ws/build/dobot/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/xenial_home/dobot_ws/src/dobot
+# Build directory: /home/xenial_home/dobot_ws/build/dobot
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
