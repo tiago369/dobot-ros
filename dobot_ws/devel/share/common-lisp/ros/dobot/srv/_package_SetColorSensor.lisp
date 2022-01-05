6e776acc263d53cccb9a2412eb4d9be14ba6ae91
tiago369 <87242204+tiago369@users.noreply.github.com>
2022-01-05 09:49:00 -0300

    Delete dobot_ws/devel directory

--- dobot_ws/devel/share/common-lisp/ros/dobot/srv/_package_SetColorSensor.lisp
+++
@@ -1,8 +0,0 @@
-(cl:in-package dobot-srv)
-(cl:export '(ENABLECTRL-VAL
-          ENABLECTRL
-          COLORPORT-VAL
-          COLORPORT
-          RESULT-VAL
-          RESULT
-))
--- dobot_ws/devel/share/dobot/cmake/dobot-msg-paths.cmake
+++
@@ -1,4 +0,0 @@
-# generated from genmsg/cmake/pkg-msg-paths.cmake.develspace.in
-
-set(dobot_MSG_INCLUDE_DIRS "/home/xenial_home/dobot_ws/src/dobot/msg")
-set(dobot_MSG_DEPENDENCIES std_msgs)
