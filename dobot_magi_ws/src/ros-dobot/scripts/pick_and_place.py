c7e42332623d7c48e559ec4bd73be9711bf87f14
tiago369 <tiagobarreto581@gmail.com>
2022-02-11 13:18:42 -0300

    a

--- dobot_magi_ws/src/ros-dobot/scripts/pick_and_place.py
+++ dobot_magi_ws/src/ros-dobot/scripts/pick_and_place.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
 import rospy
 from geometry_msgs.msg import Pose
 
@@ -9,7 +10,6 @@ from dobot.srv import SetHOMEParams
 
 if __name__ == "__main__":
     rospy.init_node('pick_and_place', anonymous=True)
-    print('1')
     suction_srv = rospy.ServiceProxy('DobotServer/SetEndEffectorSuctionCup', SetEndEffectorSuctionCup)
     get_pose = rospy.ServiceProxy('DobotServer/GetPose', GetPose)
     set_home = rospy.ServiceProxy('DobotServer/SetHOMEParams', SetHOMEParams)
@@ -21,29 +21,37 @@ if __name__ == "__main__":
     ini_x = pose.x
     ini_y = pose.y
     ini_z = pose.z
-    print('The actual pose is')
-    print('X: ', ini_x)
-    print('Y: ', ini_y)
-    print('Z: ', ini_z)
-
-    print()
-
-    print("Say where the object is positioned in the table")
-    x = int(input("X axis: "))
-    y = int(input("Y axis: "))
-    z = int(input("Z axis: "))
-    x = pose.x
-    z = pose.z
-
+    
     print('a')
+    Valores arbitrarios para delimitar a area de atuação do robo
+    while True:
+        print('The actual pose is')
+        print('X: ')
+        print(pose.x)
+        print('Y: ')
+        print(pose.y)
+        print('Z: ')
+        print(pose.z)
+        print()
+
+        print("Say where the object is positioned in the table")
+        x = int(input("X axis: "))
+        y = int(input("Y axis: "))
+        z = int(input("Z axis: "))
+        if 0 <= x > 200:
+            print('X value is not acceptble')
+        elif -100 < y > 100:
+            print('Y value is not acceptble')
+        elif -100 < z > 100:
+            print('Z value is not acceptble')
+        else:
+            break
 
     freq = rospy.Rate(10)
 
     while not rospy.is_shutdown():
         msg = Pose()
-        print('b')
         resp = suction_srv(1, 0, False)
-        print('c')
 
         print('Move ate o ponto para pegar o objeto')
         dist = 1
@@ -55,12 +63,12 @@ if __name__ == "__main__":
             dist = ((x - pose.x) + (y - pose.y) + (z - pose.z))/3
 
         print('Suga')
-        resp = suction_srv(1, 1, False)
+        resp = suction_srv(1, 254, False)
 
         dist = 1
         print('Move ate o ponto inicial')
         while dist >= 1 or not rospy.is_shutdown():
-            msg.position.x = 0
+            msg.position.x = 2
             msg.position.y = 0
             msg.position.z = 0
             publisher.publish(msg)
